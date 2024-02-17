f5d2df524ddbf130b762d0fd0f72adfaeba96423
kona <139791558+konatose@users.noreply.github.com>
2024-02-17 00:40:28 +0000

    Create Id find.py

---
+++ Id find.py
@@ -0,0 +1,11 @@
+import aminofix as amino
+
+client = amino.Client()
+client.parse_headers = lambda data=None, type=None: {**amino.headers.ApisHeaders(deviceId=amino.helpers.gen_deviceId() if client.autoDevice else client.device_id, data=data, type=type).headers, 'Host': 'service.aminoapps.com'}
+
+n=input("Profile link: ")
+fok=client.get_from_code(n)
+id=client.get_from_code(n).objectId
+cid=fok.path[1:fok.path.index("/")]
+print("this mf ndc://g/user-profile/"+id)
+#print("yoyo" +cid) #for comid￼Enter
